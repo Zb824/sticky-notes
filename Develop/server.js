156f4ba43ea4b3bd9fa51871c792cdbffe16e6bd
Zb824 <zborders28@outlook.com>
2023-02-03 16:36:06 -0600

    updates

--- Develop/server.js
+++ Develop/server.js
@@ -2,22 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const uuid = require('uuid');
-const PORT = env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 
 
 app.use(express.JSON());
 
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
 // get method
 
 app.get('/notes', (req, res) =>  {
-  res.send ('')
+  res.sendFile ('')
 })
 
 //post method
 
 app.post('/api/notes', (req, res) => {
   res.send('')
+})
+
+app.delete('api/notes', (req,res)=>{
+  
 })
 
 
